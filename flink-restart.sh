ff391c2bba50df6cccb7939b5eb1545b2587eb8f
fangyi-work <546490703@qq.com>
2019-03-19 11:07:08 +0800

    增加忽略文件, 重启脚本更新

--- flink-restart.sh
+++ flink-restart.sh
@@ -3,12 +3,12 @@
 cd /
 ./usr/local/opt/flink-1.6.0/bin/stop-cluster.sh
 
-# cd /tmp
-# rm -rf blobStore-*
-# rm -rf flink-*
-# rm -rf executionGraphStore-*
-# rm -rf jaas-*
-# rm -rf jffi*
+cd /tmp
+rm -rf blobStore-*
+rm -rf flink-*
+rm -rf executionGraphStore-*
+rm -rf jaas-*
+rm -rf jffi*
 
 cd /
 ./usr/local/opt/flink-1.6.0/bin/start-cluster.sh
