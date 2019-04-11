b9207024cc2ff92a8b97e9bfaf65e8fc70f11c82
fangyi-work <546490703@qq.com>
2019-04-11 18:16:42 +0800

    删除分支后核心代码留到master

--- app/stream/operator/test/test_operator.py
+++ app/stream/operator/test/test_operator.py
@@ -17,35 +17,36 @@ class TestOperator(OperatorBase):
 
 class MapTest(MapFunction):
     def map(self, value):
-        value = json.loads(value)
-        value = value.get('Args').get('data')[0]
-
-        DetailsTraffic = ck_table.DetailsTraffic
-        ck_store = ClickhouseStore()
-        ck_session = ck_store.get_session()
-        value_dict = {
-            'id': str(uuid.uuid4()),
-            'add_date': Func.get_date(),
-            'add_time': Func.get_date(time_format='%Y-%m-%d %H:%M:%S'),
-            'update_time': Func.get_date(time_format='%Y-%m-%d %H:%M:%S'),
-            'website': value.get('website').decode('utf-8'),
-            'plat': value.get('plat'),
-            'req_status': value.get('req_status'),
-            'agent_type': value.get('agent_type'),
-            'req_date': '2019-03-14',
-            'url': value.get('url'),
-            'visit_id': value.get('visit_id'),
-        }
-        update_dict = {
-            'host': '\'123\'',
-            'ref_url': '\'{}\''.format(str(time.time()))
-        }
-        ck_store.insert_list(ck_session, DetailsTraffic, [value_dict])
-        filter_obj = ck_session.query(DetailsTraffic).\
-            filter(DetailsTraffic.req_date == '2019-03-15').\
-            filter(DetailsTraffic.req_date <= '2019-03-16'). \
-            filter(DetailsTraffic.plat == 1)
-        ck_store.update(DetailsTraffic, filter_obj, update_dict)
-        ck_store.delete(DetailsTraffic, filter_obj)
-        ck_session.close()
-        return str(value)
+        return value
+        # value = json.loads(value)
+        # value = value.get('Args').get('data')[0]
+        #
+        # DetailsTraffic = ck_table.DetailsTraffic
+        # ck_store = ClickhouseStore()
+        # ck_session = ck_store.get_session()
+        # value_dict = {
+        #     'id': str(uuid.uuid4()),
+        #     'add_date': Func.get_date(),
+        #     'add_time': Func.get_date(time_format='%Y-%m-%d %H:%M:%S'),
+        #     'update_time': Func.get_date(time_format='%Y-%m-%d %H:%M:%S'),
+        #     'website': value.get('website').decode('utf-8'),
+        #     'plat': value.get('plat'),
+        #     'req_status': value.get('req_status'),
+        #     'agent_type': value.get('agent_type'),
+        #     'req_date': '2019-03-14',
+        #     'url': value.get('url'),
+        #     'visit_id': value.get('visit_id'),
+        # }
+        # update_dict = {
+        #     'host': '\'123\'',
+        #     'ref_url': '\'{}\''.format(str(time.time()))
+        # }
+        # ck_store.insert_list(ck_session, DetailsTraffic, [value_dict])
+        # filter_obj = ck_session.query(DetailsTraffic).\
+        #     filter(DetailsTraffic.req_date == '2019-03-15').\
+        #     filter(DetailsTraffic.req_date <= '2019-03-16'). \
+        #     filter(DetailsTraffic.plat == 1)
+        # ck_store.update(DetailsTraffic, filter_obj, update_dict)
+        # ck_store.delete(DetailsTraffic, filter_obj)
+        # ck_session.close()
+        # return str(value)
--- app/utils/constants.py
+++ app/utils/constants.py
@@ -4,26 +4,3 @@
 CK_NULL_DATETIME = '1970-01-01 00:00:00'
 CK_NULL_DATE = '1970-01-01'
 CK_NULL_ACT_DATETIME = '1970-01-01 06:28:16'
-
-# Topic Name
-TOPIC_L1_DEPOSIT = 'deposit'
-TOPIC_L1_AGGPAY = 'agg_pay'
-TOPIC_L2_NEED_STATUS = 'need_status'
-TOPIC_L2_OTHER_STATUS = 'other_status'
-
-# 支付渠道
-PAY_SOURCE_POLY = '4'  # 聚合支付
-PAY_SOURCE_ANT = '6'  # 蚂蚁钱包
-PAY_SOURCE_QUICK = '7'  # 快速转账
-PAY_SOURCE_EUPAY = '8'  # EuPay
-PAY_SOURCE_MANUAL = '3'  # 银行汇款
-
-
-# 渠道-编号对应配置
-PAY_SOURCE_FLAG_CONFIG = {
-        '[\'800001\', \'800004\']': 4,   # 聚合支付
-        '[\'800032\']': 6,             # 蚂蚁钱包
-        '[\'800128\']': 8,             # 银联转账
-        '[\'800256\']': 3,             # 手工转账/银行电汇
-        '[\'800064\']': 7,             # EuPay充值
-    }
